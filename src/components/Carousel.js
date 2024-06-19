643ac408b3e81aeadd274286e16b4af68df26fae
defk0n1 <zrigua.ghassen@gmail.com>
2024-06-19 14:22:45 +0200

    fixed assets bug

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -64,7 +64,7 @@ function Main({ radius = 1.5, count = 8 }) {
   return Array.from({ length: count }, (_, i) => (
     <Card
       key={i}
-      url={`/img${Math.floor(i % 10) + 1}_.jpg`}
+      url={`/img${Math.floor(i % 10) + 1}.jpg`}
       position={[Math.sin((i / count) * Math.PI * 2) * radius, 0, Math.cos((i / count) * Math.PI * 2) * radius]}
       rotation={[0, Math.PI + (i / count) * Math.PI * 2, 0]}
     />
